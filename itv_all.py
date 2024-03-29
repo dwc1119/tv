55bde643a21e971030d8ea476ee46af933fea889
dwc <159391114+dwc1119@users.noreply.github.com>
2024-03-29 14:03:07 +0800

    Update itv_all.py

--- itv_all.py
+++ itv_all.py
@@ -304,6 +304,19 @@ with open("itvlist.txt", 'w', encoding='utf-8') as file:
             else:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
+   file.write('数字频道,#genre#\n')
+    for result in results:
+        channel_name, channel_url, speed = result
+        if '天元，春秋，影视' in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
     channel_counters = {}
     file.write('卫视频道,#genre#\n')
     for result in results:
