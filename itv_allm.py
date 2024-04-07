d3ec002ff75be04cd946bef2bc44f0f913cbdf9a
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-07 09:05:49 +0800

    Update itv_allm.py

--- itv_allm.py
+++ itv_allm.py
@@ -180,7 +180,7 @@ for url in urls:
 
 results = set(results)   # 去重得到唯一的URL列表
 results = sorted(results)
-with open("itvlist.txt", 'w', encoding='utf-8') as file:
+with open("itv.txt", 'w', encoding='utf-8') as file:
     for result in results:
         file.write(result + "\n")
         print(result)
