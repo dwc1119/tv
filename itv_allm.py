028410fc670fcede21a62f6e10143276ce83bde3
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-09 10:03:03 +0800

    Update itv_allm.py

--- itv_allm.py
+++ itv_allm.py
@@ -10,7 +10,7 @@ from queue import Queue
 ###urls城市根据自己处地理位置修改
 urls = [
     "https://fofa.info/result?qbase64=572R57ucVFbnrqHnkIbns7vnu58%3D",
-    "https://fofa.info/result?qbase64=Iue9kee7nFRW566h55CG57O757ufIg%3D%3D"
+   # "https://fofa.info/result?qbase64=Iue9kee7nFRW566h55CG57O757ufIg%3D%3D"
 ]
 
 def modify_urls(url):
@@ -200,10 +200,11 @@ def worker():
             channel_url_t = channel_url.rstrip(channel_url.split('/')[-1])  # m3u8链接前缀
             lines = requests.get(channel_url, timeout = 1).text.strip().split('\n')  # 获取m3u8文件内容
             ts_lists = [line.split('/')[-1] for line in lines if line.startswith('#') == False]  # 获取m3u8文件下视频流后缀
-            print (ts_lists)
+            
             ts_lists_0 = ts_lists[0].rstrip(ts_lists[0].split('.ts')[-1])  # m3u8链接前缀
             ts_url = channel_url_t + ts_lists[0]  # 拼接单个视频片段下载链接
-
+            
+            print (ts_url)
             # 多获取的视频数据进行5秒钟限制
             with eventlet.Timeout(5, False):
                 start_time = time.time()
