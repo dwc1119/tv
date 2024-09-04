0fd4371c1f08228db3ddecd8f7a77aa771017c33
dwc <159391114+dwc1119@users.noreply.github.com>
2024-09-04 08:00:01 +0800

    Update c.py

--- c.py
+++ c.py
@@ -19,124 +19,67 @@ urls = [
     #"https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIHJlZ2lvbj0iaGViZWki"#河北
 ]
 
-
-def modify_urls(url):
-    modified_urls = []
-    ip_start_index = url.find("//") + 2
-    ip_end_index = url.find(":", ip_start_index)
-    base_url = url[:ip_start_index]  # http:// or https://
-    ip_address = url[ip_start_index:ip_end_index]
-    port = url[ip_end_index:]
-    ip_end = "/status"
-    for i in range(1, 256):
-        modified_ip = f"{ip_address[:-1]}{i}"
-        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
-        modified_urls.append(modified_url)
-
-    return modified_urls
-
-
-def is_url_accessible(url):
+def extract_unique_ip_ports(url):
     try:
-        response = requests.get(url, timeout=0.5)
-        if response.status_code == 200:
-            return url
-    except requests.exceptions.RequestException:
-        pass
-    return None
-
-
-results = []
-
-for url in urls:
-    try:
-        # 创建一个Chrome WebDriver实例
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-    
-        driver = webdriver.Chrome(options=chrome_options)
-        # 使用WebDriver访问网页
-        driver.get(url)  # 将网址替换为你要访问的网页地址
+        response = requests.get(url)
         time.sleep(10)
-        # 获取网页内容
-        page_content = driver.page_source
-    
-        # 关闭WebDriver
-        driver.quit()
-    
-        # 查找所有符合指定格式的网址
-        pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
-        urls_all = re.findall(pattern, page_content)
-        # urls = list(set(urls_all))  # 去重得到唯一的URL列表
-        urls = set(urls_all)  # 去重得到唯一的URL列表
-        x_urls = []
-        for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
-            url = url.strip()
-            ip_start_index = url.find("//") + 2
-            ip_end_index = url.find(":", ip_start_index)
-            ip_dot_start = url.find(".") + 1
-            ip_dot_second = url.find(".", ip_dot_start) + 1
-            ip_dot_three = url.find(".", ip_dot_second) + 1
-            base_url = url[:ip_start_index]  # http:// or https://
-            ip_address = url[ip_start_index:ip_dot_three]
-            port = url[ip_end_index:]
-            ip_end = "1"
-            modified_ip = f"{ip_address}{ip_end}"
-            x_url = f"{base_url}{modified_ip}{port}"
-            x_urls.append(x_url)
-        urls = set(x_urls)  # 去重得到唯一的URL列表
+        html_content = response.text
+        # 使用正则表达式匹配IP地址和端口号
+        ips_ports = re.findall(r'(\d+\.\d+\.\d+\.\d+:\d+)', html_content)
+        unique_ips_ports = list(set(ips_ports))  # 去除重复的IP地址和端口号
+         
+        if unique_ips_ports:
+            return unique_ips_ports
 
-        valid_urls = []
-        #   多线程获取可用url
-        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-            futures = []
-            for url in urls:
-                url = url.strip()
-                modified_urls = modify_urls(url)
-                for modified_url in modified_urls:
-                    futures.append(executor.submit(is_url_accessible, modified_url))
+    except requests.RequestException as e:
+        print(f"请求错误: {e}")
+        return None
 
-            for future in concurrent.futures.as_completed(futures):
-                result = future.result()
-                if result:
-                    valid_urls.append(result)    
+# 检查视频流的可达性
+def check_video_stream_connectivity(ip_port, urls_udp):
+    try:
+        # 构造完整的视频URL
+        video_url = f"http://{ip_port}{urls_udp}"
+        # 用OpenCV读取视频
+        cap = cv2.VideoCapture(video_url)
         
-        for url in valid_urls:
-            print(f"可用url:{url}")
-            # 创建一个Chrome WebDriver实例
-            chrome_options = Options()
-            chrome_options.add_argument('--headless')
-            chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--disable-dev-shm-usage')
+        # 检查视频是否成功打开
+        if not cap.isOpened():
+            print(f"视频URL {video_url} 无效")
+            return None
+        else:
+            # 读取视频的宽度和高度
+            width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            print(f"视频URL {video_url} 的分辨率为 {width}x{height}")
+            # 检查分辨率是否大于0
+            if width > 0 and height > 0:
+                return ip_port  # 返回有效的IP和端口
+            # 关闭视频流
+            cap.release()
+    except Exception as e:
+        print(f"访问 {ip_port} 失败: {e}")
+    return None           
+
+# 定义组播地址和端口
+urls_udp = "/rtp/239.254.200.45:8008"
+
+# 提取唯一的IP地址和端口号
+ip_ports = []
+valid_ips = []
+for url in urls:
+    ip_ports = extract_unique_ip_ports(url)
+    if ip_ports:
+        print("IP地址和端口号：")
+        print(ip_ports)
+    #测试每个IP地址和端口号，直到找到一个可访问的视频流
+    for ip_port in ip_ports:
+        valid_ip = check_video_stream_connectivity(ip_port, urls_udp)
+        if valid_ip:
+            print(f"找到可访问的视频流服务: {valid_ip}")
+            valid_ips.append(valid_ip)
         
-            driver = webdriver.Chrome(options=chrome_options)
-            # 使用WebDriver访问网页
-            driver.get(url)  # 将网址替换为你要访问的网页地址
-            time.sleep(10)
-            # 获取网页内容
-            page_content = driver.page_source
         
-            # 关闭WebDriver
-            driver.quit()
-        
-            # 查找所有符合指定格式的网址
-            #if "<td>1</td>" or "<td>2</td>" or "<td>3</td>" or "<td>4</td>"  or "<td>5</td>" or "<td>6</td>" or "<td>7</td>" or "<td>8</td>" in page_content:
-            if "6666" not in page_content and "4000" not in page_content and "5673" not in page_content:
-                print(url)
-                udpxy_urls = []# 修改文件转发地址
-                ip_start_index = url.find("//") + 2
-                ip_dot_start = url.find(".") + 1
-                ip_index_second = url.find("/", ip_dot_start)
-                base_url = url[:ip_start_index]  # http:// or https://
-                ip_address = url[ip_start_index:ip_index_second]
-                url_x = f"{base_url}{ip_address}"
-                udpxy_url = f"{url_x}"
-                results.append(udpxy_url)
-            
-    except:
-        continue
 channels = []
 with open("iptv3.txt", 'r', encoding='utf-8') as file:
     lines = file.readlines()
