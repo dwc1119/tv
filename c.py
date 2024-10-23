8292033bf27561f293a096f02f01a42872167e93
dwc <159391114+dwc1119@users.noreply.github.com>
2024-10-24 07:51:55 +0800

    Update c.py

--- c.py
+++ c.py
@@ -65,11 +65,11 @@ for url in urls:
     if ip_ports:
         print("IP地址和端口号：")
         #测试每个IP地址和端口号，直到找到一个可访问的视频流
-    for ip_port in ip_ports:
-        valid_ip = check_video_stream_connectivity(ip_port, urls_udp)
-        if valid_ip:
-            print(f"找到可访问的视频流服务: {valid_ip}")
-            valid_ips.append(valid_ip)
+        for ip_port in ip_ports:
+            valid_ip = check_video_stream_connectivity(ip_port, urls_udp)
+            if valid_ip:
+                print(f"找到可访问的视频流服务: {valid_ip}")
+                valid_ips.append(valid_ip)
         
         
 channels = []
