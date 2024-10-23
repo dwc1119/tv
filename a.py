fc4b58da0cc8df04acad6e43e06a9bcf7ad33e97
dwc <159391114+dwc1119@users.noreply.github.com>
2024-10-23 19:30:13 +0800

    Update a.py

--- a.py
+++ a.py
@@ -70,15 +70,16 @@ for url in urls:
     if ip_ports:
         print("IP地址和端口号：")
     #测试每个IP地址和端口号，直到找到一个可访问的视频流
-    try:
-        for ip_port in ip_ports:
+    
+    for ip_port in ip_ports:
+        try:
             valid_ip = check_video_stream_connectivity(ip_port, urls_udp)
             if valid_ip:
                 print(f"找到可访问的视频流服务: {valid_ip}")
                 valid_ips.append(valid_ip)
     
-    except TypeError as e:
-        print(f"发生错误：{e}")  # 打印错误信息
+        except TypeError as e:
+            print(f"发生错误：{e}")  # 打印错误信息
 
     
     
