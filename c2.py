e7f25527ef86dafa0419336af902fc7679af532e
dwc <159391114+dwc1119@users.noreply.github.com>
2024-10-27 08:44:06 +0800

    Update c2.py

--- c2.py
+++ c2.py
@@ -68,11 +68,11 @@ for url in urls:
             if valid_ip:
                 print(f"找到可访问的视频流服务: {valid_ip}")
                 valid_ips.append(valid_ip)
-channels = []
-with open("iptv3.txt", 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-    for line in lines:
-        print(line)
+                channels = []
+                with open("iptv3.txt", 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                for line in lines:
+                print(line)
        # line = line.strip()
         #if line:
            # channel_name,channel_url = line.split(",")
