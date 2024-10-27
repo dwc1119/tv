125183ae4c07af1c4a78cfed42b1030ca0e681c6
dwc <159391114+dwc1119@users.noreply.github.com>
2024-10-27 14:56:22 +0800

    Update c2.py

--- c2.py
+++ c2.py
@@ -64,7 +64,7 @@ for url in urls:
     ports = extract_unique_ip_ports(url)
     if ports:
         ip_ports = ports
-    print(ip_ports)
+        print(ip_ports)
         #file.write(f"{ip_ports}\n")
         #测试每个IP地址和端口号，直到找到一个可访问的视频流
        # for ip_port in ip_ports:
@@ -76,7 +76,7 @@ for url in urls:
              #   file.write(f"{valid_ip}\n")
             
            # print(valid_ips)
-with open("c.txt", 'a', encoding='utf-8') as file:
+#with open("c.txt", 'a', encoding='utf-8') as file:
   #  for ip_port in ip_ports:
      #   print(ip_port)
         #file.write(f"{ip_port}\n")
