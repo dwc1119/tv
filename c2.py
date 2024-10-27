f67bf2add4f618a0509871e9c40c62f3a7f66e5c
dwc <159391114+dwc1119@users.noreply.github.com>
2024-10-27 14:54:45 +0800

    Update c2.py

--- c2.py
+++ c2.py
@@ -77,8 +77,8 @@ for url in urls:
             
            # print(valid_ips)
 with open("c.txt", 'a', encoding='utf-8') as file:
-    for ip_port in ip_ports:
-        print(ip_port)
+  #  for ip_port in ip_ports:
+     #   print(ip_port)
         #file.write(f"{ip_port}\n")
 #for valid_ip in valid_ips:
     #print(valid_ip)
