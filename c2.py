213d28317a8ae750fcd3052cb7c0eac4f96b4fd1
dwc <159391114+dwc1119@users.noreply.github.com>
2024-10-27 09:50:42 +0800

    Update c2.py

--- c2.py
+++ c2.py
@@ -67,7 +67,7 @@ for url in urls:
             print(f"找到可访问的视频流服务: {valid_ip}")
             valid_ips.append(valid_ip)
             with open("c.txt", 'w', encoding='utf-8') as file:
-                file.write(valid_ip  + "\n")
+                file.write(ip_port + "\n")
 
 
 
