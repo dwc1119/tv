99645eefb78f91a30aa3e9cb9c2076e7c6d500d7
dwc <159391114+dwc1119@users.noreply.github.com>
2024-10-30 17:03:53 +0800

    Update a1.py

--- a1.py
+++ a1.py
@@ -6,13 +6,13 @@ import cv2  # 导入OpenCV库
 # 定义fofa链接
 urls = [
      "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9ImxhbmdmYW5nIg%3D%3D",#廊坊
-  #  "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9InRhbmdzaGFuIg%3D%3D",#唐山
-#    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9InNoaWppYXpodWFuZyI%3D",#石家庄
- #   "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjE0MTc3MSIgJiYgY2l0eT0iemhhbmdqaWFrb3Ui",#张家口
-  #  "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9ImhhbmRhbiI%3D",#邯郸
-  #  "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9ImNhbmd6aG91Ig%3D%3D",#沧州
-  #  "https://fofa.info/result?qbase64=InVkcHh5IDEuMC0yNS4xIiAmJiBhc249IjE0MDkwMyIgJiYgY2l0eT0iQmFvZGluZyI%3D",#保定
-   # "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjE0MDkwMyIgJiYgY2l0eT0iQmFvZGluZyI%3D"#保定
+     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9InRhbmdzaGFuIg%3D%3D",#唐山
+     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9InNoaWppYXpodWFuZyI%3D",#石家庄
+     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjE0MTc3MSIgJiYgY2l0eT0iemhhbmdqaWFrb3Ui",#张家口
+     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9ImhhbmRhbiI%3D",#邯郸
+     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9ImNhbmd6aG91Ig%3D%3D",#沧州
+     "https://fofa.info/result?qbase64=InVkcHh5IDEuMC0yNS4xIiAmJiBhc249IjE0MDkwMyIgJiYgY2l0eT0iQmFvZGluZyI%3D",#保定
+     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjE0MDkwMyIgJiYgY2l0eT0iQmFvZGluZyI%3D"#保定
 ]
 # fofa_url = 'http://tonkiang.us/hoteliptv.php?page=1&pv=%E9%87%8D%E5%BA%86%E8%81%94%E9%80%9A'
 
